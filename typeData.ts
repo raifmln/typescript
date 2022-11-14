520e191348ced3013b608ab1646f4ca77ab73e42
raif <raifmaulana29@gmail.com>
2022-11-14 18:39:52 +0700

    Add file typeData.ts

---
+++ typeData.ts
@@ -0,0 +1,9 @@
+let frontName: string = "Roronoa";
+let lastName: string = "Zoro";
+let age: number = 20;
+let salary: number = 150;
+
+console.log(`My name is ${frontName} ${lastName}`);
+console.log(`I am ${age} years old`);
+console.log(`My salary is ${salary} Berry`);
+
