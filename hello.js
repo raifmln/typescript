8726f9926eed49ec1b5830b0e827e66c4067be1f
raif <raifmaulana29@gmail.com>
2022-11-14 18:09:17 +0700

    add file hello.js

---
+++ hello.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+    return "Hello, my name is ".concat(name);
+}
+var response = sayHello("Whitebeard");
+console.log(response);
---
+++ hello.ts
@@ -0,0 +1,7 @@
+function sayHello(name: string){
+    return `Hello, my name is ${name}`;
+}
+
+let response: string = sayHello("Whitebeard");
+
+console.log(response);
