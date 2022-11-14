b6480ba7d86714b94ca785fe1e6fd5d0bd1f5de3
raif <raifmaulana29@gmail.com>
2022-11-14 18:20:23 +0700

    Add function address

--- hello.ts
+++ hello.ts
@@ -2,6 +2,12 @@ function sayHello(name: string){
     return `Hello, my name is ${name}`;
 }
 
+function address(district: string){
+    return `My District is ${district}`;
+}
+
 let response: string = sayHello("Whitebeard");
+let response2: string = address("Moby Dick");
 
 console.log(response);
+console.log(response2);
