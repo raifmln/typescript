27d98b07d6b083f2494282fc0dc13744cbb4d736
raif <raifmaulana29@gmail.com>
2022-11-15 17:36:15 +0700

    Add enum string

--- enum.ts
+++ enum.ts
@@ -3,3 +3,16 @@ enum satisfication{hate=1, dislike=2, neutral=3, like=4, love=5}
 let response: satisfication = satisfication.love;
 
 console.log(response);
+
+// enum string
+
+enum gender{
+    male="MALE",
+    female="FEMALE",
+    other="OTHER"
+}
+
+let sex: gender = gender.female
+console.log(sex);
+
+
